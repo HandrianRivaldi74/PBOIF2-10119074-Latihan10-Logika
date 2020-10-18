88e24c7afdff0d7fb6a60cb83498a6b7f8a92b50
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 14:40:43 +0700

    Latihan10

---
+++ src/pboif2_10119074_latihan10/PBOIF2_10119074_Latihan10.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan10;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Logika
+ */
+public class PBOIF2_10119074_Latihan10 {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
